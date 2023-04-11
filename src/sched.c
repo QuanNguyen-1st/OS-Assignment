400629e303caa445dde9b817dd63238618c034a4
Cyese <87186561+Cyese@users.noreply.github.com>
2023-04-11 09:54:29 +0700

    Sched skecth

--- src/sched.c
+++ src/sched.c
@@ -47,6 +47,11 @@ struct pcb_t * get_mlq_proc(void) {
 	/*TODO: get a process from PRIORITY [ready_queue].
 	 * Remember to use lock to protect the queue.
 	 * */
+	for (i = 0; i < MAX_PRIO; i ++){
+		pthread_mutex_lock(&queue_lock);
+		proc = dequeue(&mlq_ready_queue[prio]);
+		pthread_mutex_unlock(&queue_lock);
+	}
 	return proc;	
 }
 
@@ -79,6 +84,9 @@ struct pcb_t * get_proc(void) {
 	/*TODO: get a process from [ready_queue].
 	 * Remember to use lock to protect the queue.
 	 * */
+	pthread_mutex_lock(&queue_lock);
+	proc = dequeue(&ready_queue);
+	pthread_mutex_unlock(&queue_lock);
 	return proc;
 }
 
